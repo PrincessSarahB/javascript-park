a814d1c4be12416f66f911da62511f0ad4a7ddfb
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-06-05 17:11:45 +0100

    park has empty enclosure test passing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -3,16 +3,20 @@ const Dinosaur = require('../dinosaur.js');
 const Park = require('../park.js');
 
 describe('park', function(){
-  let enclosure;
+  let park;
   let dinosaur1;
   let dinosaur2;
   let dinosaur3;
 
   beforeEach(function(){
-    enclosure = new Enclosure();
+    park = new Park();
     dinosaur1 = new Dinosaur('Tyrannosaurus', 3);
     dinosaur2 = new Dinosaur('Velociraptor', 1);
     dinosaur3 = new Dinosaur('Diplodocus', 4);
+  });
+
+  it('should have empty enclosure', function(){
+    assert.strictEqual(park.enclosure.length, 0);
   });
 
 
